9b7358be379aa05efb04618068226077ed63fe93
Anurag Mishra <anurag1998mishra@gmail.com>
2020-06-20 15:23:40 +0530

    Increase the limit of article description.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 800 }
 end
