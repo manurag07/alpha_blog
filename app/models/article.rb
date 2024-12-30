a73059c63cc68e23fe08b5c583a7a758a7bfde61
Anurag <anurag1998mishra@gmail.com>
2024-12-30 13:33:28 +0530

    Refactor the unused code

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 800 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 3000 }
   has_many :article_categories, dependent: :destroy
   has_many :categories, through: :article_categories
 end
