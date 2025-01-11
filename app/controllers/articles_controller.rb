9b35a4bd3269422c270e462231ee96d9fef81f32
Anurag <anurag1998mishra@gmail.com>
2025-01-12 01:36:21 +0530

    refactor the delete button from link to button.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,7 +38,12 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article.destroy
+    if @article.destroy
+      flash[:notice] = 'Article was deleted successfully.'
+    else
+      flash[:alert] = 'Something went wrong, please try again.'
+    end
+
     redirect_to articles_path
   end
 
